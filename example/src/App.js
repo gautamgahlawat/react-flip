46388e32e01b0d881ef5f7d3f260f815e73405c3
Gautam <gautam@grappus.com>
2020-02-13 01:09:13 +0530

    Added flip functionality through porps

--- example/src/App.js
+++ example/src/App.js
@@ -3,10 +3,17 @@ import React, { Component } from "react";
 import ReactFlip from "react-flip";
 
 export default class App extends Component {
+
+  handleClick = ()=> {
+
+  }
+
+
   render() {
     return (
       <div>
-        <ReactFlip front={'front side here'} back={'back is on the back'} action={'click'}/>
+        <ReactFlip front={'front side here'} back={'back is on the back'} action={'hover'} direction={'horizontal'}/>
+        <button onClick={this.handleClick}>Press me!</button>
       </div>
     );
   }
